cb93d479323d29723ffec71c392983ce73f85698
jimb0 <jimccann@redhat.com>
2023-05-08 09:40:38 -0400

    test

--- modules/vlans/main.tf
+++ modules/vlans/main.tf
@@ -2,7 +2,7 @@ resource "ibm_network_vlan" "public" {
   name       = "${var.datacenter}-public"
   datacenter = var.datacenter
   type       = "PUBLIC"
-  router_hostname = "fcr01a.${var.datacenter}.pod04"
+  router_hostname = "fcr04.${var.datacenter}"
   tags       = var.tags
 }
 
@@ -10,6 +10,7 @@ resource "ibm_network_vlan" "private" {
   name       = "${var.datacenter}-private"
   datacenter = var.datacenter
   type       = "PRIVATE"
-  router_hostname = "bcr01a.${var.datacenter}.pod04"
+  #router_hostname = "bcr04.${var.datacenter}"
+  router_hostname = replace(ibm_network_vlan.public.router_hostname, "/^f/", "b")
   tags = var.tags
 }
