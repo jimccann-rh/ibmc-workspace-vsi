413313d6417dd72ac30cad9fa3f65d90d1bcbe08
jimb0 <jimccann@redhat.com>
2023-05-10 12:08:24 -0400

    major change added twingate

--- modules/compute-virtual/main.tf
+++ modules/compute-virtual/main.tf
@@ -39,7 +39,8 @@ resource "ibm_compute_vm_instance" "instance" {
   local_disk               = var.local_disk
   private_network_only     = false
   flavor_key_name          = local.instance_flavor
-  user_metadata            = file("${path.module}/user-data.yml")
+  user_metadata            = data.template_file.init.rendered
+#  user_metadata            = file("${path.module}/user-data.yml")
   private_vlan_id          = var.private_vlan
   public_vlan_id           = var.public_vlan
   tags                     = var.tags
